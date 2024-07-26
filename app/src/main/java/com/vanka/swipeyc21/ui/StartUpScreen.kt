51ec4788f3c95beba687fb057cd15038d538eebc
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2024-07-26 21:53:57 +0530

    first commit

---
+++ app/src/main/java/com/vanka/swipeyc21/ui/StartUpScreen.kt
@@ -0,0 +1,12 @@
+package com.vanka.swipeyc21.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vanka.swipeyc21.R
+
+class StartUpScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_up_screen)
+    }
+}
