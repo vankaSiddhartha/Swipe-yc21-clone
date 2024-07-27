c116d9080b9f2854548c5f764f3a4e3c6539f39b
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2024-07-27 14:00:31 +0530

    Bottom auth sheet updated

--- app/src/main/java/com/vanka/swipeyc21/ui/auth/OnBoardingActivity.kt
+++ app/src/main/java/com/vanka/swipeyc21/ui/auth/OnBoardingActivity.kt
@@ -3,10 +3,18 @@ package com.vanka.swipeyc21.ui.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vanka.swipeyc21.R
+import com.vanka.swipeyc21.databinding.ActivityOnBoardingBinding
+import com.vanka.swipeyc21.utils.BottomAuthSheet
 
 class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        binding.button.setOnClickListener {
+            val bottomSheetFragment = BottomAuthSheet()
+            bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
+        }
+        setContentView(binding.root)
     }
 }
